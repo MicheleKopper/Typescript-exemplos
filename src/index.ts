4166075357f28a6637e662525ee8a1b5037f089e
Michele Kopper <michelekopper@gmail.com>
2024-08-01 10:54:43 -0300

    Tipos de TS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Olá, mundo');
+
+// ------ TIPOS DE TYPESCRIPT ------
+
+// STRING
+const nome: string = "Michele Kopper";
+
+// NUMBERS
+const soma: number = 22;
+
+// BOOLEANS
+const ativo: boolean = true;
+
+// NULL
+const souNull: null = null;
+
+// UNDEFINED
+const naoDefinido: undefined = undefined;
+
+// ARRAYS
+const numeros: number[] = [2, 34, 1, 0];
+const listaMercado: Array<string> = ["Tomate", "Cebola"];
+const arrayDinamico: (string | number | boolean)[] = ["Sou string", 1, true, 1];
+
+
